2a1049fe53820e6d2cc1e79a6eec24345fb23fd5
DESKTOP-VQM4KIO\ASUS <mychikhtn@gmail.com>
2017-04-13 01:32:28 +0700

    Upload Project first time

---
+++ DoAn_Mobileshop/DoAn_Mobileshop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoAn_Mobileshop.Startup))]
+namespace DoAn_Mobileshop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
