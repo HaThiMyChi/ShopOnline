20b4bb2ca9c27d06eb83d304e121233757cdec11
DESKTOP-VQM4KIO\ASUS <mychikhtn@gmail.com>
2017-04-19 01:27:03 +0700

    ChinhSua_Product_Details_PhanTrang

---
+++ DoAn_Mobileshop/Models/BUS/MobileShopBUS.cs
@@ -0,0 +1,26 @@
+﻿using MobileShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAn_Mobileshop.Models.BUS
+{
+    public class MobileShopBUS
+    {
+        public static IEnumerable<SanPham> DanhSach()
+        {
+            var db = new MobileShopConnectionDB();
+            return db.Query<SanPham>("Select * from SanPham");
+
+        }
+
+        public static SanPham ChiTiet(int id)
+        {
+            var db = new MobileShopConnectionDB();
+            return db.SingleOrDefault<SanPham>("select * from SanPham where MaSanPham = @0", id);
+
+        }
+    }
+}
+    
